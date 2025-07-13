46a265764eaffb861ec13b63ce5ca7bcdf19ddf3
Mr.Patates <61029037+MrPatates@users.noreply.github.com>
2025-07-13 22:30:49 +0200

    FULL ADD
    augh

---
+++ js/config.js
@@ -0,0 +1,45 @@
+// ignore
+var Config = {};
+
+/**
+ * What should the text in the center of the screen be?
+ * if empty it will fill in your Server Name
+ */
+Config.title = "";
+
+/**
+ * Enable steamId text in the top right corner of the screen?
+ */
+Config.enableSteamID = true;
+
+/**
+ * Enable announcements?
+ */
+Config.enableAnnouncements = true;
+
+/**
+ * What messages do you want to show up?
+ * only works if enableAnnouncements = true
+ */
+Config.announceMessages = [
+  "Bienvenue sur Squid Game World!",
+  "Chaque joueur éliminé vous rapproche de la cagnotte finale.",
+  "https://discord.gg/squidgameworld"
+];
+
+/**
+ * How many miliseconds for each announcement?
+ * only works if enableAnnouncements = true
+ */
+Config.announcementLength = 3000;
+
+/**
+ * Image Filename
+ * DROP IMAGE IN "images" FOLDER
+ */
+Config.backgroundImage = "";
+
+/**
+ * Enable debug messages?
+ */
+Config.enableDebug = false;
